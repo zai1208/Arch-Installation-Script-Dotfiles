3b0f34a015bf059843c18ecf05263697d511c914
zai1208 <zai1208@outlook.com>
2025-08-10 21:14:53 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,10 +54,10 @@ echo "[*] UUID of encrypted partition: $ROOT_PART_UUID"
 
 # --- Pacstrap variables ---
 BASE_PACKAGES=(base base-devel linux linux-firmware man-db man-pages vim archlinux-keyring amd-ucode)
-DEV_PACKAGES=(git networkmanager)
+DEV_PACKAGES=(git)
 HYPRLAND_PACKAGES=(hyprland waybar fuzzel alacritty swww thunar gtk4 hyprlock)
 APPS_PACKAGES=(atril chromium gimp feh)
-UTIL_PACKAGES=(cups cups-pdf cups-filters cups-pk-helper pipewire pipewire-pulse pavucontrol bluez blueman)
+UTIL_PACKAGES=(cups cups-pdf cups-filters cups-pk-helper pipewire pipewire-pulse pavucontrol bluez blueman networkmanager nm-connection-editor)
 FONT_CURSOR_PACKAGES=(adwaita-cursors ttf-hack-nerd ttf-nerd-fonts-symbols)
 EXTRA_PACKAGES=(fastfetch cmatrix)
 
